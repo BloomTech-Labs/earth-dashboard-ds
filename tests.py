2f07b9c95a538d4d23763e143aa07c3ed5d2400f
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-06-08 17:02:40 -0700

    testing.

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 import unittest
 import pytest
 
-
+'''
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(3, "3")
-
+'''
 
 if __name__ == '__main__':
     unittest.main()
